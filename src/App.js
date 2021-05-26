d3fb944a7c58f430f2cb4b0d1cafde1b45bcb341
naisthorpe <aisthorpe@gmail.com>
2021-05-25 20:21:06 -0400

    Components folder set up and wrapper js/css started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import EmployeeListTable from "./components/EmployeeListTable";
+import EmployeeListItem from "./components/EmployeeListItem";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header />
+      <Search />
+      <EmployeeListTable />
+      <EmployeeListItem />
+    </Wrapper>
   );
 }
 
